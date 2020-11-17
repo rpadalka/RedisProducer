e5f1350e335d0827525ed57af57bcb95af74788b
Roman Padalka <rpadalka@Romans-Mac-mini.local>
2020-11-17 22:33:36 +0300

    Initial commit

---
+++ src/main/java/com/artezio/vtbdemo/redisproducer/RedisProducerApplication.java
@@ -0,0 +1,13 @@
+package com.artezio.vtbdemo.redisproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisProducerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artezio/vtbdemo/redisproducer/RedisProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artezio.vtbdemo.redisproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
